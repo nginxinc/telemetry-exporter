1e435e5a67cdedd4db4bf03d3a2971f4f6b2228f
Michael Pleshakov <m.pleshakov@f5.com>
2024-02-13 13:31:27 -0500

    Add exporter library
    Problem:
    We need a library to send product telemetry data as OTel
    to a remote endpoint.
    
    Solution:
    - Add library to pkg/telemetry folder
    - Add .gitignore
    - Add Makefile with go-related targets.
    
    Testing:
    - Unit test
    - Manual testing with OTel collector
      https://opentelemetry.io/docs/collector/

---
+++ pkg/telemetry/exporter_test.go
@@ -0,0 +1,123 @@
+package telemetry_test
+
+import (
+	"context"
+	"errors"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.opentelemetry.io/otel/attribute"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+
+	"github.com/nginxinc/telemetry-exporter/pkg/telemetry"
+	"github.com/nginxinc/telemetry-exporter/pkg/telemetry/telemetryfakes"
+)
+
+type exportableData struct {
+	attributes []attribute.KeyValue
+}
+
+func (d exportableData) Attributes() []attribute.KeyValue {
+	return d.attributes
+}
+
+var _ = Describe("Exporter", func() {
+	When("SpanProvider works correctly", func() {
+		var (
+			fakeSpanExporter *telemetryfakes.FakeSpanExporter
+			exporter         *telemetry.Exporter
+			data             exportableData
+		)
+
+		BeforeEach(func() {
+			fakeSpanExporter = &telemetryfakes.FakeSpanExporter{}
+			provideSpanExporter := func(_ context.Context) (sdktrace.SpanExporter, error) {
+				return fakeSpanExporter, nil
+			}
+
+			errorHandler := telemetry.NewErrorHandler()
+
+			var err error
+			exporter, err = telemetry.NewExporter(
+				telemetry.ExporterConfig{
+					SpanProvider: provideSpanExporter,
+				},
+				telemetry.WithGlobalOTelErrorHandler(errorHandler),
+			)
+
+			Expect(err).ToNot(HaveOccurred())
+
+			data = exportableData{
+				attributes: []attribute.KeyValue{
+					attribute.String("key", "value"),
+				},
+			}
+		})
+
+		When("no errors occur", func() {
+			It("exports data successfully", func() {
+				err := exporter.Export(context.Background(), data)
+
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(fakeSpanExporter.ExportSpansCallCount()).To(Equal(1))
+
+				_, res := fakeSpanExporter.ExportSpansArgsForCall(0)
+
+				Expect(res).To(HaveLen(1))
+				Expect(res[0].Attributes()).To(Equal(data.attributes))
+
+				Expect(fakeSpanExporter.ShutdownCallCount()).To(Equal(1))
+			})
+		})
+
+		When("SpanExporter returns an error", func() {
+			It("fails to export data", func() {
+				testError := errors.New("test error")
+
+				fakeSpanExporter.ExportSpansReturns(testError)
+
+				err := exporter.Export(context.Background(), data)
+
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(MatchError(testError))
+				Expect(fakeSpanExporter.ShutdownCallCount()).To(Equal(1))
+			})
+		})
+
+		AfterEach(func() {
+			err := exporter.Shutdown(context.Background())
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+
+	When("SpanProvider returns an error", func() {
+		It("fails to export data", func() {
+			testError := errors.New("test error")
+			provideSpanExporter := func(_ context.Context) (sdktrace.SpanExporter, error) {
+				return nil, testError
+			}
+
+			exporter, err := telemetry.NewExporter(
+				telemetry.ExporterConfig{
+					SpanProvider: provideSpanExporter,
+				},
+			)
+			Expect(err).ToNot(HaveOccurred())
+
+			data := exportableData{
+				attributes: []attribute.KeyValue{
+					attribute.String("key", "value"),
+				},
+			}
+
+			err = exporter.Export(context.Background(), data)
+
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(testError))
+
+			err = exporter.Shutdown(context.Background())
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
