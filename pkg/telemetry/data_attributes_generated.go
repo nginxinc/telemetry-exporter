6fef29d15aaec912edb5bba8b63c8479452261b2
Michael Pleshakov <pleshakov@users.noreply.github.com>
2024-03-05 12:40:33 -0500

    Define common data points (#27)
    Problem:
    Ensure that our Kubernetes-related projects share common telemetry
    data points for consistency.
    
    Solution:
    - Define common data points type.
    - Generate Attributes() methods to allow using the type with the
    Exporter (to implement Exportable interface).
    
    A project that uses the exporter library will also export the
    defined data type.
    
    CLOSES - https://github.com/nginxinc/telemetry-exporter/issues/8
    
    Co-authored-by: Saylor Berman <s.berman@f5.com>

--- pkg/telemetry/data_attributes_generated.go
+++ pkg/telemetry/data_attributes_generated.go
@@ -13,7 +13,14 @@ import (
 func (d *Data) Attributes() []attribute.KeyValue {
 	var attrs []attribute.KeyValue
 
-	attrs = append(attrs, attribute.Int64("Nodes", d.Nodes))
+	attrs = append(attrs, attribute.String("ProjectName", d.ProjectName))
+	attrs = append(attrs, attribute.String("ProjectVersion", d.ProjectVersion))
+	attrs = append(attrs, attribute.String("ProjectArchitecture", d.ProjectArchitecture))
+	attrs = append(attrs, attribute.String("ClusterID", d.ClusterID))
+	attrs = append(attrs, attribute.String("ClusterVersion", d.ClusterVersion))
+	attrs = append(attrs, attribute.String("ClusterPlatform", d.ClusterPlatform))
+	attrs = append(attrs, attribute.String("InstallationID", d.InstallationID))
+	attrs = append(attrs, attribute.Int64("ClusterNodeCount", d.ClusterNodeCount))
 	
 
 	return attrs
