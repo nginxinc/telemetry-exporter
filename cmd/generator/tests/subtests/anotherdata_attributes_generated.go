900ce7b11cd4f1faeec1afccefc2279af8414c60
Luca Comellini <luca.com@gmail.com>
2024-08-09 15:03:58 -0700

    Align linters with other projects

--- cmd/generator/tests/subtests/anotherdata_attributes_generated.go
+++ cmd/generator/tests/subtests/anotherdata_attributes_generated.go
@@ -1,5 +1,6 @@
 //go:build generator
 package subtests
+
 /*
 This is a generated file. DO NOT EDIT.
 */
@@ -7,14 +8,11 @@ This is a generated file. DO NOT EDIT.
 import (
 	"go.opentelemetry.io/otel/attribute"
 
-	
 	"github.com/nginxinc/telemetry-exporter/pkg/telemetry"
-	
 )
 
 func (d *AnotherData) Attributes() []attribute.KeyValue {
 	var attrs []attribute.KeyValue
-
 	attrs = append(attrs, attribute.String("AnotherSomeString", d.AnotherSomeString))
 	attrs = append(attrs, attribute.Int64("AnotherSomeInt", d.AnotherSomeInt))
 	attrs = append(attrs, attribute.Float64("AnotherSomeFloat", d.AnotherSomeFloat))
@@ -23,7 +21,6 @@ func (d *AnotherData) Attributes() []attribute.KeyValue {
 	attrs = append(attrs, attribute.Int64Slice("AnotherSomeInts", d.AnotherSomeInts))
 	attrs = append(attrs, attribute.Float64Slice("AnotherSomeFloats", d.AnotherSomeFloats))
 	attrs = append(attrs, attribute.BoolSlice("AnotherSomeBools", d.AnotherSomeBools))
-	
 
 	return attrs
 }
